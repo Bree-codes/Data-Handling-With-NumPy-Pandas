e74bb4b3be0bc7b6b4be427595ea842d98cad617
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-30 10:23:02 +0300

    Learning NumPy

---
+++ LearningNumPy/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a NumPy array of integers from 1 to 5
+array = np.array([1, 2, 3, 4, 5])
+
+# Multiply each element in the array by 2
+result = array * 2
+
+# Print the original array and the result
+print("Original array:", array)
+print("Array after multiplication:", result)
