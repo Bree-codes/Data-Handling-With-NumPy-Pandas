4d64cb543977b2476e58d945745f5e4162916a6d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-09-15 15:28:07 +0300

    Exploring pandas library

--- ExplorePandas/main.py
+++ ExplorePandas/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#DataFrame
 data = {
     'Name' : ['Steve','Bree'],
     'Age' : [21,20],
@@ -9,3 +10,17 @@ index= ['Student1','Student2']
 
 df =pd.DataFrame(data,index= index)
 print(df)
+print("\n")
+
+#Series
+data1 = {
+    'Sugar': '3 kgs',
+    'Butter': '1 kg',
+    'Flour': '2 kgs'
+}
+
+# Create a Pandas Series with the data and index
+data1 = pd.Series(data1)
+
+# Display the Series
+print(data1)
