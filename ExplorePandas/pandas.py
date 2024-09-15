5627f482d19af5ab000a6ecf379f25dd34c8352d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-09-15 14:24:51 +0300

    Exploring pandas library

---
+++ ExplorePandas/pandas.py
@@ -0,0 +1,10 @@
+from operator import index
+
+import pandas as pd
+
+data = {
+    'Name' : ['Steve','Bree'],
+    'Age' : [21,20],
+    'Course' : ['CyberSecurity','CS']
+}
+index= ['Student1','Student2']
